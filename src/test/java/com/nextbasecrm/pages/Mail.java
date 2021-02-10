43388950eee93f6f3d47fdc1507dbad2b53dce12
salihayyildiz <salihayyildiz@gmail.com>
2021-02-10 18:04:38 +0300

    trying conflict2

--- src/test/java/com/nextbasecrm/pages/Mail.java
+++ src/test/java/com/nextbasecrm/pages/Mail.java
@@ -5,7 +5,18 @@ import org.openqa.selenium.support.FindBy;
 
 public class Mail {
 
-    //logout from dropdown menu
+
+    //logout from dropdown menu--
     @FindBy(linkText = "Log out")
     public WebElement logOut;
+
+    //account username--
+    @FindBy(id = "user-name")
+    public WebElement userName;
+
+    //Tasks from the "dropdown Menu Next To Logo"--
+    @FindBy(xpath = "(//a[contains(text(), 'Tasks')])[1]")
+    public WebElement tasksFromDropdownMenu;
+
+
 }
