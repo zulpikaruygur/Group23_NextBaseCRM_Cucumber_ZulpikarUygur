29439ba3fbe34df8a2864258c4ccfd7c0cf4fed1
salihayyildiz <salihayyildiz@gmail.com>
2021-02-10 17:55:43 +0300

    trying conflict

--- src/test/java/com/nextbasecrm/pages/Mail.java
+++ src/test/java/com/nextbasecrm/pages/Mail.java
@@ -1,5 +1,11 @@
 package com.nextbasecrm.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Mail {
 
+    //logout from dropdown menu
+    @FindBy(linkText = "Log out")
+    public WebElement logOut;
 }
