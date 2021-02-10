103ac069467c5e715114234e750488e2552adea5
Yck61 <63733958+Yck61@users.noreply.github.com>
2021-02-10 17:55:33 +0300

    commit

--- src/test/java/com/nextbasecrm/pages/Mail.java
+++ src/test/java/com/nextbasecrm/pages/Mail.java
@@ -1,5 +1,12 @@
 package com.nextbasecrm.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Mail {
+
+	//account username
+	@FindBy(id = "user-name")
+	public WebElement userName;
 
 }
